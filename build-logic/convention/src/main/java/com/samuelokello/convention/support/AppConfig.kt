ca27732ea2509ebf4b3a2225fc2c475385274719
okello <samuelokello929@gmail.com>
2025-01-29 03:09:09 -0800

    Add app configurations to build logic convention - Add `AppConfig` object to define app-level configurations such as `compileSdk`, `minSdk`, `targetSdk`, and `minimumCoverageLimit`.

---
+++ build-logic/convention/src/main/java/com/samuelokello/convention/support/AppConfig.kt
@@ -0,0 +1,11 @@
+package com.samuelokello.convention.support
+
+import java.math.BigDecimal
+
+object AppConfig  {
+    const val compileSdk = 35
+    const val minSdk = 24
+    const val targetSdk = 35
+
+    val minimumCoverageLimit = BigDecimal("0.7")
+}
