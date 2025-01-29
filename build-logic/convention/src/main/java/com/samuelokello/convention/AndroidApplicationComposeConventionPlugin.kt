f186117ea77a8abacc2744b273264df1b50cc855
okello <samuelokello929@gmail.com>
2025-01-29 03:06:13 -0800

    add application compose convention plugin.

---
+++ build-logic/convention/src/main/java/com/samuelokello/convention/AndroidApplicationComposeConventionPlugin.kt
@@ -0,0 +1,20 @@
+package com.samuelokello.convention
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.samuelokello.convention.support.configureAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.samuelokello.convention.application")
+            }
+
+            val extension = extensions.getByType<ApplicationExtension>()
+            configureAndroidCompose(extension)
+        }
+    }
+}
