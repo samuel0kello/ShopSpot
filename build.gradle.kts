2907aa0663728624ce494f33154cb8fd3964e24f
Samuel Okello <samuelokello929@gmail.com>
2025-07-25 16:26:39 +0300

    build: Use version catalog for ksp plugin
    The ksp plugin version is now managed through the version catalog.
    Also, added the jetbrains.kotlin.jvm plugin alias.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
