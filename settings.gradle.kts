b515bed634ecca880cf01c9758623f606c0bf4a6
Samuel Okello <samuelokello929@gmail.com>
2025-07-26 13:24:31 +0300

    set-up core:domain module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,13 @@ dependencyResolutionManagement {
 
 rootProject.name = "ShopSpot"
 include(":app")
+include(":core")
+//include(":core:data")
+include(":core:data")
+include(":core:datasource")
+include(":core:datasource:local")
+include(":core:datasource:remote")
+include(":core:domain")
+include(":core:presentation")
+include(":core:presentation:designsystem")
+include(":core:presentation:ui")
