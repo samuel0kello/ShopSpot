fb4b7d56120724d40e636e5ba4e6594c03d7abd5
Samuel Okello <samuelokello929@gmail.com>
2025-07-31 09:04:22 +0300

    Create HttpClientUtil for network requests
    This commit introduces `HttpClientUtil.kt`, which provides utility functions for making GET, POST, and DELETE requests using Ktor HttpClient.
    
    The utility includes:
    - Extension functions for `HttpClient` to simplify GET, POST, and DELETE operations.
    - `safeApiCall` function to wrap API calls, handling common exceptions and mapping HTTP response codes to `Result<Res, DataError.Network>`.
    - `handleApiException` to categorize network-related exceptions (e.g., `UnresolvedAddressException`, `SerializationException`) into specific `DataError.Network` types.
    - `mapResponseCodes` to translate HTTP status codes into corresponding `DataError.Network` results.
    - A placeholder `createRoute` function.

---
+++ core/datasource/remote/src/main/java/com/example/remote/util/HttpClientUtill.kt
@@ -0,0 +1,93 @@
+package com.example.remote.util
+
+import android.util.Log
+import com.example.core.domain.util.DataError
+import com.example.core.domain.util.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlinx.serialization.SerializationException
+import kotlin.coroutines.coroutineContext
+
+internal suspend inline fun <reified Res : Any> HttpClient.get(
+    path: String,
+    queryParams: Map<String, Any?>  = emptyMap()
+): Result<Res, DataError.Network> {
+    return safeApiCall {
+        get {
+            url( createRoute(path))
+            queryParams.map { (key, value) ->
+                parameter(key,value)
+            }
+        }
+    }
+}
+
+internal suspend inline fun <reified Req : Any, reified Res : Any> HttpClient.post(
+    path: String,
+    body: Req
+): Result<Res, DataError.Network> {
+    return safeApiCall {
+        post {
+            url( createRoute(path))
+            setBody(body)
+        }
+    }
+}
+
+internal suspend inline fun <reified Res : Any> HttpClient.delete(
+    path: String,
+    queryParams: Map<String, Any?>  = emptyMap()
+): Result<Res, DataError.Network> {
+    return safeApiCall {
+        delete {
+            url( createRoute(path))
+            queryParams.map { (key, value) ->
+                parameter(key,value)
+            }
+        }
+    }
+}
+
+internal suspend inline fun <reified Res : Any> safeApiCall(func: () -> HttpResponse): Result<Res, DataError.Network> {
+    return try {
+        mapResponseCodes( func())
+    } catch (e: Exception) {
+        Log.e("error", "$e")
+        handleApiException(e)
+    }
+}
+
+internal suspend fun handleApiException(e: Exception): Result.Error<DataError.Network> {
+    return when (e) {
+        is UnresolvedAddressException -> Result.Error(DataError.Network.NO_INTERNET)
+        is SerializationException -> Result.Error(DataError.Network.SERIALIZATION_ERROR)
+        else -> {
+            coroutineContext.ensureActive()
+            Result.Error(DataError.Network.UNKNOWN)
+        }
+    }
+}
+
+internal suspend inline fun <reified Res : Any> mapResponseCodes(response: HttpResponse): Result<Res, DataError.Network> {
+    return when(response.status.value) {
+        in 200 .. 299 -> Result.Success(response.body())
+        402 -> Result.Error(DataError.Network.UNAUTHORIZED)
+        408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
+        409 -> Result.Error(DataError.Network.CONFLICT)
+        413 -> Result.Error(DataError.Network.PAYLOAD_TOO_LARGE)
+        429 -> Result.Error(DataError.Network.TOO_MANY_REQUESTS)
+        in 500 .. 599 -> Result.Error(DataError.Network.SERVER_ERROR)
+        else -> Result.Error(DataError.Network.UNKNOWN)
+    }
+}
+
+internal fun createRoute(path: String): String =""
