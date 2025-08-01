5ed25619611b09b3263ffad386d0e43ddf6f6182
Samuel Okello <samuelokello929@gmail.com>
2025-08-01 12:02:14 +0300

    feat: Implement AuthenticationLocalSource
    This commit introduces the `AuthenticationLocalSource` interface and its implementation, `AuthenticationLocalSourceImpl`.
    
    `AuthenticationLocalSource` defines methods for managing authentication tokens:
    - `saveAccessToken(token: String)`
    - `saveRefreshToken(token: String)`
    - `getAccessToken(): Flow<String?>`
    - `getRefreshToken(): Flow<String?>`
    - `clearTokens()`
    
    `AuthenticationLocalSourceImpl` utilizes `PreferenceHelper` to store and retrieve access and refresh tokens using `PreferenceKeys.ACCESS_TOKEN` and `PreferenceKeys.REFRESH_TOKEN`.

---
+++ core/datasource/local/src/main/java/com/example/datasource/local/source/auth/AuthenticationLocalSource.kt
@@ -0,0 +1,38 @@
+package com.example.datasource.local.source.auth
+
+import com.example.datasource.local.source.preference.PreferenceHelper
+import com.example.datasource.local.source.preference.PreferenceKeys
+import kotlinx.coroutines.flow.Flow
+
+interface AuthenticationLocalSource {
+    suspend fun saveAccessToken(token: String)
+
+    suspend fun saveRefreshToken(token: String)
+
+    fun getAccessToken(): Flow<String?>
+
+    fun getRefreshToken(): Flow<String?>
+
+    suspend fun clearTokens()
+}
+
+class AuthenticationLocalSourceImpl(
+    private val remotePreferencesHelper: PreferenceHelper,
+) : AuthenticationLocalSource {
+    override suspend fun saveAccessToken(token: String) {
+        remotePreferencesHelper.save(PreferenceKeys.ACCESS_TOKEN, token)
+    }
+
+    override suspend fun saveRefreshToken(token: String) {
+        remotePreferencesHelper.save(PreferenceKeys.REFRESH_TOKEN, token)
+    }
+
+    override fun getAccessToken(): Flow<String?> = remotePreferencesHelper.get(PreferenceKeys.ACCESS_TOKEN)
+
+    override fun getRefreshToken(): Flow<String?> = remotePreferencesHelper.get(PreferenceKeys.REFRESH_TOKEN)
+
+    override suspend fun clearTokens() {
+        remotePreferencesHelper.delete(PreferenceKeys.ACCESS_TOKEN)
+        remotePreferencesHelper.delete(PreferenceKeys.REFRESH_TOKEN)
+    }
+}
