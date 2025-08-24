b7e09021ee80a46af99adaaf8437ab6702450f4b
Samuel Okello <samuelokello929@gmail.com>
2025-08-24 16:50:14 +0300

    Refactor: Update MainActivity to use core presentation components
    This commit updates `MainActivity.kt` to utilize components from the `:core:presentation` module.
    
    Specifically:
    - `ShopSpotAppNavHost` is now imported from `com.samuelokello.core.presentation.ui.navigation.ShopSpotAppNavHost`.
    - `ShopSpotTheme` is now imported from `com.samuelokello.core.presentation.designsystem.ShopSpotTheme`.
    - The `ShopSpotTheme` composable is updated to use the named `content` parameter.

--- app/src/main/java/com/samuelokello/shopspot/MainActivity.kt
+++ app/src/main/java/com/samuelokello/shopspot/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.samuelokello.shopspot.ui.navigation.ShopSpotAppNavHost
-import com.samuelokello.shopspot.ui.theme.ShopSpotTheme
+import com.samuelokello.core.presentation.designsystem.ShopSpotTheme
+import com.samuelokello.core.presentation.ui.navigation.ShopSpotAppNavHost
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
@@ -14,9 +14,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ShopSpotTheme {
+            ShopSpotTheme(content = {
                 ShopSpotAppNavHost()
-            }
+            })
         }
     }
 }
