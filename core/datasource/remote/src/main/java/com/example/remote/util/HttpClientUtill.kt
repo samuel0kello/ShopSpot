1aa2359aa59bfa453959093ac7c464c3e412295b
Samuel Okello <samuelokello929@gmail.com>
2025-08-24 16:56:49 +0300

    Revert "Refactor: Move NavigationViewModel to core module"
    This reverts commit f029da8f47b02af7e111ed3a8da665b3c9f5c601.

--- core/datasource/remote/src/main/java/com/example/remote/util/HttpClientUtill.kt
+++ core/datasource/remote/src/main/java/com/example/remote/util/HttpClientUtill.kt
@@ -1,8 +1,8 @@
-package com.samuelokello.remote.util
+package com.example.remote.util
 
 import android.util.Log
-import com.samuelokello.core.domain.util.DataError
-import com.samuelokello.core.domain.util.Result
+import com.example.core.domain.util.DataError
+import com.example.core.domain.util.Result
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.delete
