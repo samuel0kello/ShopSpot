d7ace20adc6c824733b6afdcb33a01e39d474ec3
Samuel Okello <samuelokello929@gmail.com>
2025-08-24 16:09:31 +0300

    Refactor: Create dedicated `favourite` feature module
    This commit introduces a new feature module named `favourite`.
    
    Key changes:
    - Created `feat/favourite/build.gradle.kts` with dependencies on `:core:domain` and `:core:presentation:designsystem`.
    - Moved `FavouriteScreen.kt` from the `app` module to `feat/favourite/src/main/java/com/samuelokello/feat/favourite`.
    - Created an empty Koin module `favouritesModule` in `feat/favourite/src/main/java/com/samuelokello/feat/favourite/favouritesModule.kt`.
    - Added a `.gitignore` file to the `feat/favourite` module to ignore the `build` directory.
    - Created `feat/favourite/src/main/AndroidManifest.xml`.

--- feat/favourite/src/main/java/com/samuelokello/feat/favourite/FavouriteScreen.kt
+++ feat/favourite/src/main/java/com/samuelokello/feat/favourite/FavouriteScreen.kt
@@ -1,4 +1,4 @@
-package com.samuelokello.shopspot.ui.favourite
+package com.samuelokello.feat.favourite
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
---
+++ feat/favourite/src/main/java/com/samuelokello/feat/favourite/favouritesModule.kt
@@ -0,0 +1,6 @@
+package com.samuelokello.feat.favourite
+
+import org.koin.dsl.module
+
+val favouritesModule =
+    module {}
